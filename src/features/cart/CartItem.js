7277a9133a864f4cd194f12392acafa9e521fb1f
Eunike Pardede <eunikehedriani@gmail.com>
2024-04-17 10:14:21 +0200

    edit add to wishlist button on cart item

--- src/features/cart/CartItem.js
+++ src/features/cart/CartItem.js
@@ -2,12 +2,22 @@ import { Button, Row, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { removeItem, decrementQuantity, incrementQuantity } from './CartSlice';
 import { useDispatch } from 'react-redux';
-import { addFavorite } from '../favorites/FavoritesSlice';
+import { addFavorite, removeFavorite } from '../favorites/FavoritesSlice';
+import { useState } from 'react';
 
 const CartItem = ({ item }) => {
     const { id, image, name, price, category, quantity } = item;
 
+    //replace button Love
+    const [replaceButton, setReplaceButton] = useState(true);
+
     const dispatch = useDispatch();
+
+    const handleAddWishlist = () => {
+        dispatch(addFavorite(item));
+        setReplaceButton(!replaceButton);
+    }
+
 
     return (
         <Row className='mb-2'>
@@ -24,30 +34,44 @@ const CartItem = ({ item }) => {
                 <p style={{ fontSize: '16px', color: 'black' }}>{name}</p>
             </Col>
             <Col >
-                <Button pill onClick={() => dispatch(decrementQuantity(id))} style={{display: 'inline-block',backgroundColor:'#e1e1e0',border: 'none'}}>-</Button>
-                <div md='4' className='mx-1' style={{ fontSize: '14px', display:'inline-block'}} >{quantity} {(quantity > 1) ? 'items ' : 'item '}</div>
-                <Button pill onClick={() => dispatch(incrementQuantity(id))} style={{display:'inline-block',backgroundColor:'#e1e1e0', border:'none'}}>+</Button>
+                <Button pill onClick={() => dispatch(decrementQuantity(id))} style={{ display: 'inline-block', backgroundColor: '#e1e1e0', border: 'none' }}>-</Button>
+                <div md='4' className='mx-1' style={{ fontSize: '14px', display: 'inline-block' }} >{quantity} {(quantity > 1) ? 'items ' : 'item '}</div>
+                <Button pill onClick={() => dispatch(incrementQuantity(id))} style={{ display: 'inline-block', backgroundColor: '#e1e1e0', border: 'none' }}>+</Button>
             </Col>
-            <Col style={{fontSize: '13px'}}>
-                <p md='2' className='mb-5 text-muted' style={{textAlign: 'end', fontSize: '16px'}}>€ {price}</p>
-                <div 
-                    style={{alignItems: 'end', cursor: 'pointer'}} 
+            <Col style={{ fontSize: '13px' }}>
+                <p md='2' className='mb-5 text-muted' style={{ textAlign: 'end', fontSize: '16px' }}>€ {price}</p>
+                <div
+                    style={{ alignItems: 'end', cursor: 'pointer' }}
                     onClick={() => dispatch(removeItem(id))}
                 >
-                    <FontAwesomeIcon 
-                        icon="fa-regular fa-trash-can" 
+                    <FontAwesomeIcon
+                        icon="fa-regular fa-trash-can"
                         className="removeItem"
                     /> Remove
                 </div>
-                <div 
-                    style={{alignItems: 'end', cursor: 'pointer'}}
-                    onClick={() => dispatch(addFavorite(item))}
-                >
-                    <FontAwesomeIcon 
-                        icon="fa-regular fa-heart" 
-                        className="addFavorite"
-                    /> Add to wish list
-                </div>
+                <Row>
+                    {replaceButton ?
+                        <div
+                            style={{ alignItems: 'end', cursor: 'pointer' }}
+                            onClick={handleAddWishlist}
+                        >
+                            <FontAwesomeIcon
+                                icon="fa-regular fa-heart"
+                                className="addFavorite"
+                            /> Add to wish list
+                        </div>
+                        : <div
+                            style={{ alignItems: 'end'}}
+                        >
+                            <FontAwesomeIcon
+                                icon="fa-solid fa-heart"
+                                className="addFavorite"
+                            />
+                        </div>
+                    }
+                </Row>
+
+
             </Col>
         </Row>
     )
